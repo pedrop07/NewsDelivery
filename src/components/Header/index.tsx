38c924205ce641da3978c98f5224f4f03ab83264
Pedro <pedrofulfaro@gmail.com>
2023-07-12 00:12:46 -0300

    feat: Add anchor to create-news page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,23 @@
 import { NavLink } from 'react-router-dom'
 import { Logo } from '../Logo'
+import { PlusCircle } from 'phosphor-react'
 
 export function Header() {
   return (
-    <header className="bg-gray-950 border-b border-slate-300/10 px-3 py-7 sticky top-0">
-      <div className="flex justify-between max-w-screen-xl mx-auto">
+    <header className="bg-gray-900 border-b border-slate-300/10 px-3 py-2 md:py-7 sticky top-0">
+      <div className="flex justify-between items-center max-w-screen-xl mx-auto">
         <NavLink to="/" title="Home">
           <Logo />
         </NavLink>
 
-        <div className="flex gap-3">
-          <div>teste</div>
+        <div>
+          <NavLink
+            to={'/create-news'}
+            className="flex items-center p-2 rounded-lg text-white hover:bg-gray-700 group"
+          >
+            <PlusCircle size={30} />
+            <span className="ml-2">Criar nova notícia</span>
+          </NavLink>
         </div>
       </div>
     </header>
